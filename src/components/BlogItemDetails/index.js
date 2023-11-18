91b64dfdf62f9c2eef04d3ff05c4699641feb859
tejababu846 <tejababu08@gmail.com>
2023-11-18 05:39:26 +0000

    Initial commit

---
+++ src/components/BlogItemDetails/index.js
@@ -0,0 +1,63 @@
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import './index.css'
+
+class BlogItemDetails extends Component {
+  state = {blogData: {}, isLoading: true}
+
+  componentDidMount() {
+    this.getBlogItemData()
+  }
+
+  getBlogItemData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    const updatedData = {
+      imageUrl: data.image_url,
+      title: data.title,
+      author: data.author,
+      avatarUrl: data.avatar_url,
+      content: data.content,
+      id: data.id,
+      topic: data.topic,
+    }
+
+    this.setState({blogData: updatedData, isLoading: false})
+  }
+
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+    const {isLoading} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+    return isLoading ? (
+      <div className="spinnerLoader">
+        <Loader type="TailSpin" height={50} width={50} color="#00BFFF" />
+      </div>
+    ) : (
+      <div className="blog-info">
+        <h2 className="blog-details-title">{title}</h2>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    return <div className="blog-container">{this.renderBlogItemDetails()}</div>
+  }
+}
+
+export default BlogItemDetails
